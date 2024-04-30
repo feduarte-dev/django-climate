a117c44651779bcc84e999b523d0af287748cfb4
Felipe Duarte <feduarte.dev@gmail.com>
2024-04-30 17:26:01 -0300

    requisito 2

--- weather/models.py
+++ weather/models.py
@@ -15,3 +15,27 @@ class City(models.Model):
     def save(self, *args, **kwargs):
         self.name = self.name.title()
         super(City, self).save(*args, **kwargs)
+
+
+class DailyWeather(models.Model):
+    DESCRIPTION_CHOICES = [
+        ("Ensolarado", "Ensolarado"),
+        ("Nublado", "Nublado"),
+        ("Chuvoso", "Chuvoso"),
+        ("Parcialmente nublado", "Parcialmente nublado"),
+        ("Neve", "Neve"),
+        ("Granizo", "Granizo"),
+    ]
+
+    city = models.ForeignKey(
+        City, on_delete=models.CASCADE, unique_for_date="date"
+    )
+    date = models.DateField()
+    min_temp = models.FloatField()
+    max_temp = models.FloatField()
+    brief_description = models.CharField(
+        max_length=20, choices=DESCRIPTION_CHOICES
+    )
+
+    def __str__(self) -> str:
+        return f"{self.date.strftime('%d/%m/%Y')} - {self.brief_description}"
