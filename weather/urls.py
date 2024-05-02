098d993ac84053b1a77cf336cb63e60335801f75
Felipe Duarte <feduarte.dev@gmail.com>
2024-05-02 07:27:30 -0300

    requisito 5

--- weather/urls.py
+++ weather/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from weather.views import index
+from weather.views import index, weather
 
-urlpatterns = [path("", index, name="home-page")]
+urlpatterns = [
+    path("", index, name="home-page"),
+    path("weather/<str:city>", weather, name="city-weather"),
+]
--- weather/views.py
+++ weather/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
-from weather.models import City
+from weather.models import City, DailyWeather
 
 
 def index(request):
     context = {"cities": City.objects.all()}
     return render(request, "home.html", context)
+
+
+def weather(request, city):
+    city_name = f"{city.title().replace('-', ' ')}"
+    city_query = City.objects.get(name=city_name)
+    city_weathers = DailyWeather.objects.filter(city=city_query)
+
+    context = {"weathers": city_weathers}
+    return render(request, "city_weather.html", context)
