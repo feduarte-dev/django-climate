60e5ff5c37fcc7373580d482b2d81f20b3888beb
Felipe Duarte <feduarte.dev@gmail.com>
2024-05-02 07:05:11 -0300

    requisito 3

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from weather.models import City
+
+
+def index(request):
+    context = {"cities": City.objects.all()}
+    return render(request, "home.html", context)
